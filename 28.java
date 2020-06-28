7385b7f9e71ecd293b70f1ce8f669a26b4e1fd13
Muskan Bansal <44235014+muskan18@users.noreply.github.com>
2020-06-28 22:38:53 +0530

    Day 28

---
+++ 28.java
@@ -0,0 +1,3 @@
+/* Given a list of airline tickets represented by pairs of departure and arrival airports [from, to], reconstruct the itinerary in order.
+All of the tickets belong to a man who departs from JFK. Thus, the itinerary must begin with JFK.  */
+//=======================================Code====================
